598585ba89118e21c33dbc8c5338be232f46399d
Julio <you@example.com>
2025-09-12 12:25:46 -0300

    -app.tsx

--- .netlify/functions-serve/create-checkout-session/netlify/functions/create-checkout-session.js
+++ .netlify/functions-serve/create-checkout-session/netlify/functions/create-checkout-session.js
@@ -1,2 +1,36 @@
 "use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// netlify/functions/create-checkout-session.ts
+var create_checkout_session_exports = {};
+__export(create_checkout_session_exports, {
+  handler: () => handler
+});
+module.exports = __toCommonJS(create_checkout_session_exports);
+var handler = async () => {
+  return {
+    statusCode: 404,
+    body: JSON.stringify({ error: "This function is not implemented." })
+  };
+};
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  handler
+});
 //# sourceMappingURL=create-checkout-session.js.map
